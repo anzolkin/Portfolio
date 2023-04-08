660049da36ec555eac5e811ed03d0cc74669752b
Andrey Zolkin <inform@aitishnik.ru>
2023-04-08 10:37:55 +0300

    update

--- pages/caption_zolkin_rf/js/scripts.js
+++ pages/caption_zolkin_rf/js/scripts.js
@@ -1,18 +1,1 @@
-/** Countdown Timer **/
-
-$(document).ready(function() {
-	/*"use strict";*/
-	function startCountdown(){
-		$("#countdowncont").countdown({
-			date: "28 may 2023 20:00:00", /** Enter new date here **/
-			format: "on"
-		},
-		function() {
-			// callback function
-		});
-		setTimeout(startCountdown,1000);
-	}
-	startCountdown();
-	
-});
-
+$(document).ready(function(){!function n(){$(".countdowncont").countdown({date:"28 may 2023 20:00:00",format:"on"},function(){}),setTimeout(n,1e3)}()});
